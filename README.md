5ee9810565871450d4dd417fc69e918fb613422c
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-10-08 15:21:55 +0200

    feat: Add Flutter generated project

---
+++ README.md
@@ -0,0 +1,2 @@
+# DEV_epicture_2020
+A photo finder / browsing app
---
+++ android/app/src/main/kotlin/fr/bonnet_mathieux/epicture/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.bonnet_mathieux.epicture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
